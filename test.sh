af2e75be0e9740e57a47adcbaad42c8594f3489f
Gerrit Garbereder <g.garbereder@gmail.com>
2016-06-02 08:20:15 +0200

    separated directory structure

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 vendor/bin/phpmd src text cleancode,codesize,controversial,design,naming,unusedcode
-vendor/bin/phpunit --bootstrap bootstrap.php tests/MachineTest.php 
+vendor/bin/phpunit --bootstrap bootstrap.php test/MachineTest.php 
