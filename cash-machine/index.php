f56536f7a104a2f0671a3f223fa07d7c0acd2635
Lucas Franco <lucas@lucasfranco.com.br>
2017-02-19 17:03:51 -0300

    Remove commented lines

--- cash-machine/index.php
+++ cash-machine/index.php
@@ -28,23 +28,3 @@
     }
 
     echo "\n\n";
-    // echo "\033[32m some colored text \033[37m some white text \n";die;
-
-    // $atm = new Atm;
-    // echo $atm->withdrawCash(30) . PHP_EOL . PHP_EOL;
-    // echo $atm->withdrawCash(80) . PHP_EOL . PHP_EOL;
-    // echo $atm->withdrawCash(125) . PHP_EOL . PHP_EOL;
-    // echo $atm->withdrawCash(-130) . PHP_EOL . PHP_EOL;
-    // echo $atm->withdrawCash(null) . PHP_EOL . PHP_EOL;
-
-    // echo $atm->withdrawCash('250') . PHP_EOL . PHP_EOL;
-    // echo $atm->withdrawCash(400) . PHP_EOL . PHP_EOL;
-    // echo $atm->withdrawCash(110) . PHP_EOL . PHP_EOL;
-    // echo $atm->withdrawCash('60') . PHP_EOL . PHP_EOL;
-    // echo $atm->withdrawCash(90) . PHP_EOL . PHP_EOL;
-
-    // echo $atm->withdrawCash('13.5') . PHP_EOL . PHP_EOL;
-    // echo $atm->withdrawCash(20.50) . PHP_EOL . PHP_EOL;
-    // echo $atm->withdrawCash('lucas') . PHP_EOL . PHP_EOL;
-    // echo $atm->withdrawCash() . PHP_EOL . PHP_EOL;
-    // echo $atm->withdrawCash(250010) . PHP_EOL . PHP_EOL;
