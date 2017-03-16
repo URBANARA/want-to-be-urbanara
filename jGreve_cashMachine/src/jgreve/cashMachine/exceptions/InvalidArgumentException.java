a6309ab4e99b9c67282fe2d57d9a5aae2193ddf5
Jonathan Greve <jonathangreve@Jonathans-MacBook-Pro-2.local>
2017-03-16 09:58:48 +0200

    Jonathan Greve Test Project

---
+++ jGreve_cashMachine/src/jgreve/cashMachine/exceptions/InvalidArgumentException.java
@@ -0,0 +1,12 @@
+package jgreve.cashMachine.exceptions;
+
+/**
+ * Created by jonathangreve on 2017/03/16.
+ */
+public class InvalidArgumentException extends Exception {
+
+    public InvalidArgumentException(String message) {
+        super(message);
+    }
+
+}
