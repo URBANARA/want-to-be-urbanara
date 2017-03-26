7727be24a8c1750ce2db214120cf8b79586f3649
Yaroslav Tikhomirov <yartikh@gmail.com>
2017-03-26 15:40:17 +0500

    Implementation of cash machine

---
+++ tests/solutions/CashMachine/src/Exception/NoteUnavailableException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Yatikh\CashMachine\Exception;
+
+use Exception;
+
+/**
+ * @author Yaroslav Tikhomirov <yartikh@gmail.com>
+ */
+class NoteUnavailableException extends Exception
+{}
